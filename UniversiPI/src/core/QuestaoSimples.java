feba574e571f874eb6097183bd8713b172d38978
gsgodoy <gustavosouzagodoy@hotmail.com>
2021-02-02 14:09:50 -0300

    ultimo commit

---
+++ UniversiPI/src/core/QuestaoSimples.java
@@ -0,0 +1,27 @@
+package core;
+
+public class QuestaoSimples {
+	protected String enunciado;
+	protected String resposta;
+	
+	public QuestaoSimples(String enunciado, String resposta) {
+		super();
+		this.enunciado = enunciado;
+		this.resposta = resposta;
+	}
+	
+	public String aplicarQuestao() {
+		return "Q: "+this.enunciado;
+	}
+	
+	public boolean corrigir(String respUsuario) {
+		return respUsuario.equals(resposta);
+	}
+	
+	
+	public String getResposta() {
+		return resposta;
+	}
+	
+	
+}
